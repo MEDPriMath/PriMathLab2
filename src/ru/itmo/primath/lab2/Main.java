347b14a9210ead04858cf42ee4df01860feff22a
Danilaa56 <danila.aronov@gmail.com>
2022-04-18 20:14:11 +0300

    initial PriMath lab-2 commit #3

---
+++ src/ru/itmo/primath/lab2/Main.java
@@ -0,0 +1,9 @@
+package ru.itmo.primath.lab2;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Here we go again");
+    }
+
+}
---
+++ test/ru/itmo/primath/lab2/EssentialsTest.java
@@ -0,0 +1,14 @@
+package ru.itmo.primath.lab2;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class EssentialsTest {
+
+    @Test
+    public void jupiterTest() {
+        assertEquals(4, 2 + 2);
+    }
+
+}
